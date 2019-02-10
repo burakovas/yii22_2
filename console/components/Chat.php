811c8e03cf31c968d2e1bb84a7a61aca0145cea0
burakovas <burakovas@MacBook-Air-burakovas.local>
2019-02-10 21:37:19 +0300

    10/02 wsocket

---
+++ console/components/Chat.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: burakovas
+ * Date: 2019-02-10
+ * Time: 18:04
+ */
+
+class Chat
+{
+
+}
---
+++ console/controllers/WsController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: burakovas
+ * Date: 2019-02-10
+ * Time: 17:43
+ */
+
+class WsServer
+{
+
+}
